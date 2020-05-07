96ce55320f40203535c2ee0d0562ce19bfb2eba8
Tamizhvendan S <tamizh88@gmail.com>
2020-05-07 22:12:02 +0530

    bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.graphqlize/graphqlize-java "0.1.0-alpha15"
+(defproject org.graphqlize/graphqlize-java "0.1.0-alpha16"
   :description "Java wrapper for graphqlize library"
   :url "https://github.com/graphqlize/graphqlize-java"
   :license {:name "EPL-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.graphqlize/graphqlize "0.1.0-alpha16"]]
+                 [org.graphqlize/graphqlize "0.1.0-alpha17"]]
   :java-source-paths ["java"]
   :profiles {:test {:dependencies [[org.postgresql/postgresql "42.2.10"]]}})
